51aa795c7ec53a1ec485149864f3689f1ed7e6f7
Nishant Malik <nishantmalik2015@.com>
2024-02-21 21:41:45 +0530

    dfghhgfd

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyAJfQXj7aA831YZ4B1TcFJiFXq1a0QMSdk',
+  authDomain: 'chatgpt-clone-3b6bf.firebaseapp.com',
+  projectId: 'chatgpt-clone-3b6bf',
+  storageBucket: 'chatgpt-clone-3b6bf.appspot.com',
+  messagingSenderId: '253392611921',
+  appId: '1:253392611921:web:46c7995861c1b321456670',
+  measurementId: 'G-YM3M26LFR5',
 };
 
 // Initialize Firebase
